90b3004afb487957e20e43779cc42ad0a8f2021e
Andrey_Demidov <a_demidov91@mail.ru>
2022-08-08 07:31:06 +0300

    initial Seminar4 tasks

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральную степень B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int stepen = A;
+for (int i=1; i<B; i++){
+    stepen = stepen*A;
+}
+Console.WriteLine(" Итоговый результат: " + stepen);
---
+++ Task29/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] arr = new int[8];
+for (int i = 0; i < arr.Length; i++){
+    Console.Write($"Введите элемент данного массива: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Данный массив: [" + string.Join(", ", arr) + "]");
